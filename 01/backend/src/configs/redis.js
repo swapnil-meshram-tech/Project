34084066bb40a5b89b469d6de5bc3c79780d58fa
swapnil-meshram-tech <177468871+swapnil-meshram-tech@users.noreply.github.com>
2026-08-25 15:43:47 +0530

    upload

--- 01/backend/src/configs/redis.js
+++ 01/backend/src/configs/redis.js
@@ -5,26 +5,30 @@ let redis = null
 
 const getRedis = () => {
     if(!redis) {
-        
         redis = new Redis({
             host: config.REDIS_HOST,
             port: config.REDIS_PORT,
             password: config.REDIS_PASSWORD,
-            maxRetriesPerRequest: 3
+            
+            maxRetriesPerRequest: 3,
+
+            retryStrategy(times) {
+                return Math.min(Math.pow(2, times - 1) * 50, 2000)
+            }
         })
 
         redis.on('connect', () => console.log('Redis connected.'))
+        redis.on('ready', () => console.log('Redis ready.'))
         
-        redis.on('error', (err) => {
-            console.error('Redis error:', err.message)
-            process.exit(1)
-        })
+        redis.on('error', (err) => console.error('Redis error:', err.message))
+        redis.on('reconnecting', (delay) => console.error(`Redis reconnecting in ${delay} ms.`))
+        redis.on('end', () => console.error('Redis connection end.'))
     }
 
     return redis
 }
 
-const connectRedis = async () => {
+const connectRedis = async() => {
     try {
         await getRedis().ping() 
 
