f4e9ebc3ff2c94863bc8c0dff546e9508c5876b6
swapnil-meshram-tech <177468871+swapnil-meshram-tech@users.noreply.github.com>
2026-09-15 23:48:48 +0530

    upload

--- ai/src/ai/main.py
+++ ai/src/ai/main.py
@@ -1,10 +1,39 @@
-from fastapi import FastAPI
+# from typing import TypeVar
+
+from fastapi import FastAPI, status
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
-def home():
-    return {
-        "message": "AI server is running."
-    }
+# T = TypeVar("T")
 
+
+class CreateUser(BaseModel):
+    username: str
+    email: str
+    password: str
+
+
+class UserResponse(BaseModel):
+    username: str
+    email: str
+
+
+class Response[T](BaseModel):
+    status: str
+    message: str
+    data: T
+
+
+@app.get("/")
+def home():
+    return {"message": "AI server is running."}
+
+
+@app.post(
+    "/create-user",
+    response_model=Response[UserResponse],
+    status_code=status.HTTP_200_OK,
+)
+def create_user(user: CreateUser):
+    return {"status": "success", "message": "User created", "data": user}
